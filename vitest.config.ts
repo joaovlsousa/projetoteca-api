5d6c17ec52fd81f2291ef9bc6cc316a165d99cef
João Vitor <joaov.lmsousa@gmail.com>
2026-01-06 17:49:51 -0300

    chore: remove coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     reporters: 'verbose',
-    coverage: {
-      enabled: true,
-      provider: 'v8',
-      reporter: ['text', 'text-summary', 'html'],
-      all: true,
-      include: ['src/**/*.ts'],
-      exclude: ['**/*.test.ts', 'src/tests/**'],
-    },
     alias: {
       '@core': resolve(__dirname, './src/core'),
       '@domain': resolve(__dirname, './src/domain'),
